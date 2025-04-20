82450edc81b5f18dbf313ee34455da5d08d4ea4a
Leo0114 <leonel0614@gmail.com>
2025-04-20 00:16:38 -0600

    ft:seo+

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import tailwindcss from "@tailwindcss/vite";
-
 import sitemap from "@astrojs/sitemap";
-
 import react from "@astrojs/react";
 
 // https://astro.build/config
@@ -18,6 +15,8 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
+
+  site: "https://leonelweb.vercel.app",
 
   integrations: [sitemap(), react()],
 });
